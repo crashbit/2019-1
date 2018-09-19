b5f79deb8fdfd514467d93ce1542d2555dde819c
German Santos <germans@unam.mx>
2018-09-19 16:53:23 -0500

    Collection View

--- MPOO-GPO03/AutoLayout/AutoLayout/ViewController.swift
+++ MPOO-GPO03/AutoLayout/AutoLayout/ViewController.swift
@@ -10,15 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var etiqueta : UILabel = {
+        let lb = UILabel()
+        lb.text = "Label"
+        lb.font = UIFont(name: "Arial", size: 40.0)
+        lb.translatesAutoresizingMaskIntoConstraints = false
+        return lb
+    }()
+    
+    var boton : UIButton = {
+        let bt = UIButton()
+        bt.setTitle("Apuntar score", for: .normal)
+        bt.backgroundColor = UIColor.red
+        bt.translatesAutoresizingMaskIntoConstraints = false
+       return bt
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(etiqueta)
+        view.addSubview(boton)
+        etiqueta.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        etiqueta.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        boton.topAnchor.constraint(equalTo: etiqueta.bottomAnchor, constant: 20).isActive = true
+        boton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
---
+++ MPOO-GPO03/CollectionView/CollectionView/ItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCollectionViewCell.swift
+//  CollectionView
+//
+//  Created by Germán Santos Jaimes on 9/19/18.
+//  Copyright © 2018 iosLab. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+}
