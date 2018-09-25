92d07d82a27005cf7f6f09d071d1436d4c9a8421
German Santos <germans@unam.mx>
2018-09-25 16:51:28 -0500

    CollectionItunes

---
+++ MPOO-GPO04/CollectionTracks/CollectionTracks/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  CollectionTracks
+//
+//  Created by Germán Santos Jaimes on 9/25/18.
+//  Copyright © 2018 iosLab. All rights reserved.
+//
+
+import Foundation
---
+++ MPOO-GPO04/CollectionTracks/CollectionTracks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionTracks
+//
+//  Created by Germán Santos Jaimes on 9/25/18.
+//  Copyright © 2018 iosLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
