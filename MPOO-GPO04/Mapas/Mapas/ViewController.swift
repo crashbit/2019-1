7133dc3feb82a1562c5f163d5021c795a2818bea
German Santos <germans@unam.mx>
2018-10-09 16:43:48 -0500

    Actualizado

--- MPOO-GPO04/Mapas/Mapas/ViewController.swift
+++ MPOO-GPO04/Mapas/Mapas/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapita: MKMapView!
+    let locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius : Double = 1000
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapita.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
     }
-
-
 }
 
+extension ViewController : MKMapViewDelegate{
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        mapita.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension ViewController : CLLocationManagerDelegate{
+    func configureLocationServices(){
+        if authorizationStatus == .notDetermined{
+            print("Pidiendo permisos")
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("Permisos autorizados")
+        centerMapOnUserLocation()
+    }
+    
+}
